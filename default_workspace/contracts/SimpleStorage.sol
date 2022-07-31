84fc8d1642ac2b8d5685bee11787184a02b86731
divesh-giri <dgcodes05@gmail.com>
2022-07-31 21:18:06 +0530

    first commit

---
+++ default_workspace/contracts/SimpleStorage.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8; // 
+
+// EVM, Ethereum Virtual Machine
+// Avalanche, Fantom, Ploygon
+
+contract SimpleStorage {
+    // boolean, strings, uint, int, address, bytes : Data Types
+    uint256 favouriteNumber; 
+
+    // Structure
+        // People public person = People({favouriteNumber: 2, name: "Divesh"});
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+    People[] public people;
+
+    // Mapping
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+
+    // FUNCTIONS
+    function store(uint256 _favouriteNumber) public virtual {
+            favouriteNumber = _favouriteNumber;
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    
+    // calldata, memory, storage
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+
+        people.push(People({favouriteNumber: _favouriteNumber, name: _name}));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+
+}
+
+// 0xd9145CCE52D386f254917e481eB44e9943F39138
